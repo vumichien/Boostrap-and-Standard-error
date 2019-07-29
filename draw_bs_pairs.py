3f5a816c652c286d9ed822186ab49c92357f0428
vumichien <31467068+vumichien@users.noreply.github.com>
2019-07-29 12:51:24 +0900

    Add files via upload

---
+++ draw_bs_pairs.py
@@ -0,0 +1,17 @@
+def draw_bs_pairs(x, y, func, size=1):
+    """Perform pairs bootstrap for a single statistic."""
+    """Checking the relationship between two set of data"""
+
+    # Set up array of indices to sample from: inds
+    inds = np.arange(len(x))
+
+    # Initialize replicates: bs_replicates
+    bs_replicates = np.empty(size)
+
+    # Generate replicates
+    for i in range(size):
+        bs_inds = np.random.choice(inds,size = len(inds))
+        bs_x, bs_y = x[bs_inds], y[bs_inds]
+        bs_replicates[i] = func(bs_x,bs_y)
+
+    return bs_replicates
