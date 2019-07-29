da4456d3603bfdb7b4e7e533bef66ee500aaf128
vumichien <31467068+vumichien@users.noreply.github.com>
2019-07-29 13:14:25 +0900

    Update draw_one_sample_bootstrap.py

--- draw_one_sample_bootstrap.py
+++ draw_one_sample_bootstrap.py
@@ -1,11 +1,11 @@
 def draw_one_sample_bootstrap(sample_a, test_mean):
-# Make an array of translated impact forces: translated_force_b
+# Make an array of translated sample_a
     translated_sample_a = sample_a-force_a.mean() + test_mean
 
-# Take bootstrap replicates of Frog B's translated impact forces: bs_replicates
+# Take bootstrap replicates of sample_a's translated impact: bs_replicates
     bs_replicates = draw_bs_reps(translated_sample_a, np.mean, 10000)
 
-# Compute fraction of replicates that are less than the observed Frog B force: p
+# Compute fraction of replicates that are less than the observed sample_a: p
     p = np.sum(bs_replicates <= np.mean(sample_a)) / 10000
 
 # Print the p-value
