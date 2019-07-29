06c7d7e07a93eae63e4564410fb4ea8be53a8555
vumichien <31467068+vumichien@users.noreply.github.com>
2019-07-29 13:21:22 +0900

    Add files via upload

---
+++ draw_two_samples_bootstrap.py
@@ -0,0 +1,23 @@
+"""A two-sample bootstrap hypothesis test for difference of means"""
+import numpy as np
+
+def draw_one_sample_bootstrap(sample_a, sample_b):
+# Compute mean of all sample: mean_sample, empirical_diff_means
+    sample_concat = np.concatenate(sample_a,sample_b, axis = 0)
+    mean_sample = sample_concat.mean()
+
+    empirical_diff_means = np.mean(sample_a) - np.mean(sample_b)
+# Generate shifted arrays
+    sample_a_shifted = sample_a - np.mean(sample_a) + mean_force
+    sample_b_shifted = sample_b - np.mean(sample_b) + mean_force
+
+# Compute 10,000 bootstrap replicates from shifted arrays
+    bs_replicates_a = draw_bs_reps(sample_a_shifted, np.mean, 10000)
+    bs_replicates_b = draw_bs_reps(sample_b_shifted, np.mean, 10000)
+
+# Get replicates of difference of means: bs_replicates
+    bs_replicates = bs_replicates_a -bs_replicates_b
+
+# Compute and print p-value: p
+    p = np.sum(bs_replicates >= empirical_diff_means) / 10000
+    print('p-value =', p)
